f7618987bad59dd540df5d3d1da3b280f4ce0144
agungtirt4 <60907381+agungtirt4@users.noreply.github.com>
2020-03-05 11:09:48 +0700

    first init

---
+++ file.js
@@ -0,0 +1,45 @@
+/* console.log('Halo');
+ 1. menampilkan kata 'halo'
+cara run nya ketik node namafile.js
+=======================================
+*/
+
+/*
+console.log(__dirname);
+//8. menampilkan folder directory path dengan dirname
+
+console.log(__filename);
+//8. menampilkan isi file path dengan filename
+=============================================
+*/
+
+/*//9. menampilkan isi variabel Andi 32 dan true
+var nama = 'Andi';
+console.log(nama);
+
+var usia;
+usia = 22;
+usia = 32;
+console.log(usia);
+
+let jomblo = true;
+console.log(jomblo);
+=======================================
+*/
+
+/*10. perbedaan var dan let
+var x = 21
+var x = 22
+console.log(x);
+// output nya 22 dan var x yang akan di ambil nilai terakhir
+let y = 'hai'
+let y = 'halo'
+console.log(y);
+// hasil SyntaxError karna let tidak bisa lebih dari satu variable yang sama jika tidak pakai kutip maka menggunakan boolean jika pakai kutip maka menggunakan string
+========================================
+*/
+
+
+
+
+
